1af2f56c44b8c92eadf2c1e7bd5f96af1dab8257
Emeya <emeyappdeveloper@hotmail.com>
2020-09-17 11:44:29 -0500

    Initial Commit

---
+++ TheMoviesMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMoviesMVP
+//
+//  Created by Manuel Soberanis on 17/09/20.
+//  Copyright © 2020 Manuel Soberanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
